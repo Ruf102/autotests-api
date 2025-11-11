3e38f3fee88e1b812b9b725ec74f136606d7a472
Ruslan Valeev <prodigy97@icloud.com>
2025-11-11 21:33:42 +0500

    Знакомство с gRPC протоколом. Практика работы с gRPC протоколом

---
+++ grpc_course_client.py
@@ -0,0 +1,10 @@
+import  grpc
+
+import course_service_pb2
+import course_service_pb2_grpc
+
+chanel = grpc.insecure_channel('localhost:50051')
+stub = course_service_pb2_grpc.CourseServiceStub(chanel)
+
+response = stub.GetCourse(course_service_pb2.GetCourseRequest(course_id="api-course"))
+print(response)
