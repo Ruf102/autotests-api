bd78f7a2bbe563dc350e14b15592990618252b32
Ruslan Valeev <prodigy97@icloud.com>
2025-11-11 21:38:08 +0500

    Знакомство с gRPC протоколом. Практика работы с gRPC протоколом

--- grpc_course_server.py
+++ grpc_course_server.py
@@ -5,7 +5,7 @@ import course_service_pb2_grpc
 
 class CourseServiceServicer(course_service_pb2_grpc.CourseServiceServicer):
     def GetCourse(self, request, context):
-        print(f'Получим запрос к методу GetCourse от пользователя {request.course_id}')
+        print(f'Получен запрос на получения курса по ID: {request.course_id}')
         return course_service_pb2.GetCourseResponse(course_id=f"{request.course_id}",
             title="Автотесты API", description="Будем изучать написание API автотестов")
 
