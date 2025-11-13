c021b95193669f2ae71cdecf006854835e4f75be
Ruslan Valeev <prodigy97@icloud.com>
2025-11-13 19:31:41 +0500

    Практикуемся в работе с HTTPX. Практикуемся в работе с HTTPX: обновление пользователя

---
+++ httpx_update_user.py
@@ -0,0 +1,38 @@
+import httpx
+import faker
+
+# Заполнение тела запроса рандомными данными использую библиотеку faker
+create_user_payload = {
+    "email": faker.Faker().email(),
+    "password": faker.Faker().password(),
+    "lastName": faker.Faker().last_name(),
+    "firstName": faker.Faker().first_name(),
+    "middleName": faker.Faker().first_name()
+}
+# Выполнение запроса на создание пользователя
+create_user_response = httpx.post("http://localhost:8000/api/v1/users", json=create_user_payload)
+create_user_response_data = create_user_response.json()
+print('Create user data:', create_user_response_data)
+
+# Получение токена авторизации
+login_payload = {
+  "email": create_user_payload['email'],
+  "password": create_user_payload['password']
+}
+login_response = httpx.post("http://localhost:8000/api/v1/authentication/login", json=login_payload)
+login_response_data = login_response.json()
+print('Login data:', login_response_data)
+
+# Обновление пользователя
+update_user_payload = {
+    "email": faker.Faker().email(),
+    "password": faker.Faker().password(),
+    "lastName": faker.Faker().last_name(),
+    "firstName": faker.Faker().first_name(),
+    "middleName": faker.Faker().first_name()
+}
+client = httpx.Client(headers={"Authorization": f'Bearer {login_response_data["token"]["accessToken"]}'})
+update_user_response = client.patch(f"http://localhost:8000/api/v1/users/{create_user_response_data['user']['id']}", json=update_user_payload)
+
+print(f"Update user data: {update_user_response.json()}")
+print(f"Status code: {update_user_response.status_code}")
