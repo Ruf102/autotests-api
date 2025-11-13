3059abfcfd0dcc86f578c6620ef677959bd8b38a
Ruslan Valeev <prodigy97@icloud.com>
2025-11-13 15:26:04 +0500

    ведение в HTTPX. рактическое задание: работа с HTTPX

---
+++ httpx_get_user_me.py
@@ -0,0 +1,23 @@
+import httpx
+
+# Вводим данные для входа в систему
+login_payload = {
+    "email": "user@example.com",
+    "password": "12345"
+}
+
+login_response = httpx.post("http://localhost:8000/api/v1/authentication/login", json=login_payload)
+login_response_data = login_response.json()
+
+# print("Status code:",  login_response.status_code)
+# print("login response:", login_response_data)
+
+headers = {
+    "Authorization": f'Bearer {login_response_data["token"]["accessToken"]}'
+}
+
+me_response = httpx.get("http://localhost:8000/api/v1/users/me", headers=headers)
+me_response_data = me_response.json()
+
+print("user info:", me_response_data)
+print("Status Code:", me_response.status_code)
