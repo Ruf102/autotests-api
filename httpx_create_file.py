3ce3866ac31dbef9d3918a6e1c3e74f4e2016d98
Ruslan Valeev <prodigy97@icloud.com>
2025-11-16 21:40:22 +0500

    Практикуемся в реализации API клиентов. Практика реализации API клиентов: /api/v1/files. Часть 1 — Загрузка файла

---
+++ httpx_create_file.py
@@ -0,0 +1,35 @@
+import httpx
+import faker
+
+# Создаем пользователя
+create_user_payload = {
+    "email": faker.Faker().email(),
+    "password": faker.Faker().password(),
+    "lastName": faker.Faker().last_name(),
+    "firstName": faker.Faker().first_name(),
+    "middleName": faker.Faker().first_name()
+}
+create_user_response = httpx.post("http://localhost:8000/api/v1/users", json=create_user_payload)
+create_user_response_data = create_user_response.json()
+print('Create user data:', create_user_response_data)
+
+
+login_payload = {
+  "email": create_user_payload['email'],
+  "password": create_user_payload['password']
+}
+login_response = httpx.post("http://localhost:8000/api/v1/authentication/login", json=login_payload)
+login_response_data = login_response.json()
+print('Login data:', login_response_data)
+
+create_file_headers = {
+    "Authorization": f'Bearer {login_response_data["token"]["accessToken"]}'
+}
+create_file_response = httpx.post(
+    "http://localhost:8000/api/v1/files",
+    data={"filename": "image.jpg", "directory": "test"},
+    files={"upload_file": open("./testdata/files/image.jpg", "rb")},
+    headers=create_file_headers
+)
+create_file_response_data = create_file_response.json()
+print('Create file data:', create_file_response_data)
