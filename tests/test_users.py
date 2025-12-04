98e7cab5a829020d83bee152a6084cbcc04a3c46
Ruslan Valeev <prodigy97@icloud.com>
2025-12-04 13:32:11 +0500

    Фикстуры Pytest. практика написания автотестов с использованием фикстур

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,7 +1,8 @@
 from http import HTTPStatus
 import pytest
-from clients.users.public_users_client import get_public_users_client
+from clients.users.private_users_client import PrivateUsersClient
 from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, GetUserResponseSchema
+from tests.conftest import UserFixture
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
 from tools.assertions.users import assert_create_user_response, assert_get_user_response
@@ -23,11 +24,11 @@ def test_create_user(public_users_client):
 
 @pytest.mark.users
 @pytest.mark.regression
-def test_get_user_me(private_users_client, function_user):
+def test_get_user_me(private_users_client: PrivateUsersClient, function_user: UserFixture):
     response = private_users_client.get_user_me_api()
     response_data = GetUserResponseSchema.model_validate_json(response.text)
 
     assert_status_code(response.status_code, HTTPStatus.OK)
-    assert_get_user_response(function_user.response, response_data)
+    assert_get_user_response(response_data, function_user.response)
     validate_json_schema(response.json(), response_data.model_json_schema())
 
--- tools/assertions/users.py
+++ tools/assertions/users.py
@@ -1,4 +1,5 @@
-from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, GetUserResponseSchema
+from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, GetUserResponseSchema, \
+    UserSchema
 from tools.assertions.base import assert_equal
 
 
@@ -16,32 +17,28 @@ def assert_create_user_response(request: CreateUserRequestSchema, response: Crea
     assert_equal(response.user.middle_name, request.middle_name, "middle_name")
 
 
-def assert_user(request: CreateUserResponseSchema, response: GetUserResponseSchema):
+def assert_user(actual: UserSchema, expected: UserSchema):
     """
-    Проверяет, что поля пользователя в ответе на получение (GetUserResponseSchema)
-    соответствуют полям пользователя в ответе на создание (CreateUserResponseSchema).
+    Проверяет, что фактические данные пользователя соответствуют ожидаемым.
 
-    :param request: Ответ от запроса на создание пользователя (CreateUserResponseSchema).
-    :param response: Ответ от запроса на получение пользователя (GetUserResponseSchema).
-    :raise AssertionError: Если хотя бы одно поле не совпадает.
+    :param actual: Фактические данные пользователя.
+    :param expected: Ожидаемые данные пользователя.
+    :raises AssertionError: Если хотя бы одно поле не совпадает.
     """
-    assert_equal(response.user.id, request.user.id, "id")
-    assert_equal(response.user.email, request.user.email, "email")
-    assert_equal(response.user.last_name, request.user.last_name, "last_name")
-    assert_equal(response.user.first_name, request.user.first_name, "first_name")
-    assert_equal(response.user.middle_name, request.user.middle_name, "middle_name")
+    assert_equal(actual.id, expected.id, "id")
+    assert_equal(actual.email, actual.email, "email")
+    assert_equal(actual.last_name, actual.last_name, "last_name")
+    assert_equal(actual.first_name, actual.first_name, "first_name")
+    assert_equal(actual.middle_name, actual.middle_name, "middle_name")
 
 
-def assert_get_user_response(request: CreateUserResponseSchema, response: GetUserResponseSchema):
+def assert_get_user_response(get_user_response: GetUserResponseSchema,
+        create_user_response: CreateUserResponseSchema):
     """
-    Проверяет, что ответ на получение пользователя (GetUserResponseSchema)
-    соответствует данным, которые были использованы для его создания
-    (представленным в CreateUserResponseSchema).
+    Проверяет, что ответ на получение пользователя соответствует ответу на его создание.
 
-    Использует внутреннюю функцию assert_user для сравнения основных полей пользователя.
-
-    :param request: Ответ от запроса на создание пользователя, содержащий ожидаемые данные пользователя.
-    :param response: Фактический ответ от запроса на получение пользователя.
-    :raise AssertionError: Если данные пользователя в ответах не совпадают.
+    :param get_user_response: Ответ API при запросе данных пользователя.
+    :param create_user_response: Ответ API при создании пользователя.
+    :raises AssertionError: Если данные пользователя не совпадают.
     """
-    assert_user(request, response)
+    assert_user(get_user_response.user, create_user_response.user)
