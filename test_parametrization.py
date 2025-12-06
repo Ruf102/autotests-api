f0cca87fc40f04a369096ee8df7e45b641a454ca
Ruslan Valeev <prodigy97@icloud.com>
2025-12-06 17:42:07 +0500

    Параметризация Pytest. Практика работы с pytest параметризацией

--- test_parametrization.py
+++ test_parametrization.py
@@ -37,3 +37,13 @@ users = {
 def test_identification(phone_number):
     pass
 
+@pytest.mark.parametrize(
+    "input_value",
+    [
+        pytest.param(1, marks=pytest.mark.xfail(reason="Known issue with 1")),
+        2,
+        pytest.param(3, marks=pytest.mark.skip(reason="Feature not implemented for 3")),
+    ]
+)
+def test_function(input_value):
+    assert input_value != 1
