b62e2ea1061efe65ed158cba9914b54cf992b032
Ruslan Valeev <prodigy97@icloud.com>
2025-12-06 17:44:15 +0500

    Параметризация Pytest. Практика работы с pytest параметризацией

--- clients/users/users_schema.py
+++ clients/users/users_schema.py
@@ -21,13 +21,13 @@ class CreateUserRequestSchema(BaseModel):
     """
     model_config = ConfigDict(populate_by_name=True)
 
-    email: EmailStr = Field(default_factory=fake.email())
+    email: EmailStr = Field(default_factory=fake.email)
     password: str = Field(default_factory=fake.password)
     last_name: str = Field(alias="lastName", default_factory=fake.last_name)
     first_name: str = Field(alias="firstName", default_factory=fake.first_name)
     middle_name: str = Field(alias="middleName", default_factory=fake.middle_name)
 
-class gCreateUserResponseSchema(BaseModel):
+class CreateUserResponseSchema(BaseModel):
     """
     Описание структуры ответа создания пользователя.
     """
