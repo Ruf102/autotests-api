3bd40bef61d94eaec7bc782ec886c73bc312f493
Ruslan Valeev <prodigy97@icloud.com>
2026-01-21 20:07:32 +0500

    9.2 Практикуемся в написании API автотестов. Часть 1

--- clients/files/files_schema.py
+++ clients/files/files_schema.py
@@ -25,3 +25,9 @@ class CreateFileResponseSchema(BaseModel):
     Описание структуры ответа создания файла.
     """
     file: FileSchema
+
+class GetFileResponseSchema(BaseModel):
+    """
+    Описание структуры ответа на запрос на получения файла.
+    """
+    file: FileSchema
