5f0c5ae92df07993bb36735a07409185d2b4fd17
Ruslan Valeev <prodigy97@icloud.com>
2026-01-24 15:27:45 +0500

    Практикуемся в написании API автотестов. Часть 3. Практика написания API автотестов для работы с заданиями.

--- tools/assertions/exercises.py
+++ tools/assertions/exercises.py
@@ -1,5 +1,24 @@
-from clients.exercises.exercises_schema import CreateExercisesRequestSchema, CreateExercisesResponseSchema
+from clients.exercises.exercises_schema import CreateExercisesRequestSchema, CreateExercisesResponseSchema, \
+    ExerciseSchema, GetExerciseResponseSchema
 from tools.assertions.base import assert_equal
+
+def assert_exercise(actual: ExerciseSchema, expected: ExerciseSchema):
+    """
+    Проверяет, что фактические данные задания соответствуют ожидаемым.
+
+    :param actual: Фактические данные задания.
+    :param expected: Ожидаемые данные задания.
+    :raises AssertionError: Если хотя бы одно поле не совпадает.
+    """
+    assert_equal(actual.id, expected.id, "id")
+    assert_equal(actual.title, expected.title, "title")
+    assert_equal(actual.course_id, expected.course_id, "course_id")
+    assert_equal(actual.max_score, expected.max_score, "max_score")
+    assert_equal(actual.min_score, expected.min_score, "min_score")
+    assert_equal(actual.order_index, expected.order_index, "order_index")
+    assert_equal(actual.description, expected.description, "description")
+    assert_equal(actual.estimated_time, expected.estimated_time, "estimated_time")
+
 
 def assert_create_exercise_response(request: CreateExercisesRequestSchema, response: CreateExercisesResponseSchema):
     """
@@ -17,4 +36,15 @@ def assert_create_exercise_response(request: CreateExercisesRequestSchema, respo
     assert_equal(request.description, response.exercise.description, "description")
     assert_equal(request.estimated_time, response.exercise.estimated_time, "estimated_time")
 
+def assert_get_exercise_response(
+        get_exercise_response: GetExerciseResponseSchema,
+        create_exercise_response: CreateExercisesResponseSchema
+):
+    """
+    Проверяет, что ответ на получения задания соответствует ответу на его создание.
 
+    :param get_exercise_response: Ответ API при запросе данных задания.
+    :param create_exercise_response: Ответ API при создании задания.
+    :raises AssertionError: Если данные файла не совпадают.
+    """
+    assert_exercise(create_exercise_response.exercise, get_exercise_response.exercise)
